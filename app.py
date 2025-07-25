1b5cd331466a7d029b60e4d35c575421af6e0ad6
minekaiju <YOUREMAIL@example.com>
2025-07-25 14:24:54 -0400

    Final version with correct port for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 import os
 
@@ -16,4 +17,6 @@ def status():
     }
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
